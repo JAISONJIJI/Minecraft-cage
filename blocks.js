783b5d58c0e9c90667e03391bbd6fbb0e4ca30a4
JAISONJIJI <99042969+JAISONJIJI@users.noreply.github.com>
2022-02-11 10:14:13 -0800

    Create blocks.js

---
+++ blocks.js
@@ -0,0 +1,39 @@
+player.say("placing stone")
+blocks.place(BEDROCK,  pos(0,0,1))
+blocks.place(BEDROCK, pos(0,0,2))
+blocks.place(BEDROCK, pos(0,0,3))
+blocks.place(BEDROCK, pos(0,0,4))
+blocks.place(BEDROCK, pos(0,0,5))
+blocks.place(BEDROCK, pos(0,0,6))
+//what   action  type   l/r  u/d f/b
+
+//bottom
+blocks.place(BEDROCK, pos(-5,0,1))
+blocks.place(BEDROCK, pos(-4,0,1))
+blocks.place(BEDROCK, pos(-3,0,1))
+blocks.place(BEDROCK, pos(-2,0,1))
+blocks.place(BEDROCK, pos(-1,0,1))
+//length
+blocks.place(BEDROCK, pos(0,1,1))
+blocks.place(BEDROCK, pos(0,2,1))
+blocks.place(BEDROCK, pos(0,3,1))
+blocks.place(BEDROCK, pos(0,4,1))
+blocks.place(BEDROCK, pos(0,5,1))
+//wall
+blocks.place(BEDROCK, pos(-5,1,1))
+blocks.place(BEDROCK, pos(-4,1,1))
+blocks.place(BEDROCK, pos(-3,1,1))
+blocks.place(BEDROCK, pos(-2,1,1))
+blocks.place(BEDROCK, pos(-1,1,1))
+//wall2
+blocks.place(BEDROCK, pos(-5,2,1))
+blocks.place(BEDROCK, pos(-4,2,1))
+blocks.place(BEDROCK, pos(-3,2,1))
+blocks.place(BEDROCK, pos(-2,2,1))
+blocks.place(BEDROCK, pos(-1,2,1))
+//wall3
+blocks.place(BEDROCK, pos(-5,3,1))
+blocks.place(BEDROCK, pos(-4,3,1))
+blocks.place(BEDROCK, pos(-3,3,1))
+blocks.place(BEDROCK, pos(-2,3,1))
+blocks.place(BEDROCK, pos(-1,3,1))
