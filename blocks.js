9b603dc56463d6753fc0207dd03cdba93af43346
JAISONJIJI <99042969+JAISONJIJI@users.noreply.github.com>
2022-03-04 11:06:16 -0800

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -1,11 +1,13 @@
-player.say("placing bedrock")
+player.onChat("start", function() {
+   
+player.say("order placed")
 blocks.place(BEDROCK,  pos(0,0,1))
 blocks.place(BEDROCK, pos(0,0,2))
 blocks.place(BEDROCK, pos(0,0,3))
 blocks.place(BEDROCK, pos(0,0,4))
 blocks.place(BEDROCK, pos(0,0,5))
 blocks.place(BEDROCK, pos(0,0,6))
-//what   action  type   l/r  u/d f/b
+//what   action  type   l/r u/d f/b
 
 //bottom
 blocks.place(BEDROCK, pos(-5,0,1))
@@ -161,6 +163,7 @@ blocks.place(BEDROCK, pos(-0,3,3))
 blocks.place(BEDROCK, pos(-0,2,3))
 blocks.place(BEDROCK, pos(-0,1,3))
 
+player.say("half way done Job")
 
 blocks.place(BEDROCK, pos(-0,4,2))
 blocks.place(BEDROCK, pos(-0,3,2))
@@ -173,9 +176,44 @@ blocks.place(BEDROCK, pos(-0,2,1))
 blocks.place(BEDROCK, pos(-0,1,1))
 blocks.place(BEDROCK, pos(-0,0,1))
 
-blocks.place(BEDROCK, pos(-0,4,6))
-blocks.place(BEDROCK, pos(-0,3,6))
-blocks.place(BEDROCK, pos(-0,2,6))
-blocks.place(BEDROCK, pos(-0,1,6))
-blocks.place(BEDROCK, pos(-0,0,6))
-player.say("done placing bedrock");
+blocks.place(BEDROCK, pos(-0,4,5))
+blocks.place(BEDROCK, pos(-0,3,5))
+blocks.place(BEDROCK, pos(-0,2,5))
+blocks.place(BEDROCK, pos(-0,1,5))
+blocks.place(BEDROCK, pos(-0,0,5))
+
+blocks.place(BEDROCK, pos(-5,4,5))
+blocks.place(BEDROCK, pos(-5,3,5))
+blocks.place(BEDROCK, pos(-5,2,5))
+blocks.place(BEDROCK, pos(-5,1,5))
+blocks.place(BEDROCK, pos(-5,0,5))
+
+blocks.place(BEDROCK, pos(-5,4,4))
+blocks.place(BEDROCK, pos(-5,3,4))
+blocks.place(BEDROCK, pos(-5,2,4))
+blocks.place(BEDROCK, pos(-5,1,4))
+blocks.place(BEDROCK, pos(-5,0,4))
+
+blocks.place(BEDROCK, pos(-5,4,3))
+blocks.place(BEDROCK, pos(-5,3,3))
+blocks.place(BEDROCK, pos(-5,2,3))
+blocks.place(BEDROCK, pos(-5,1,3))
+blocks.place(BEDROCK, pos(-5,0,3))
+
+blocks.place(BEDROCK, pos(-5,4,2))
+blocks.place(BEDROCK, pos(-5,3,2))
+blocks.place(BEDROCK, pos(-5,2,2))
+blocks.place(BEDROCK, pos(-5,1,2))
+blocks.place(BEDROCK, pos(-5,0,2))
+
+blocks.place(BEDROCK, pos(-5,4,1))
+blocks.place(BEDROCK, pos(-5,3,1))
+blocks.place(BEDROCK, pos(-5,2,1))
+blocks.place(BEDROCK, pos(-5,1,1))
+blocks.place(BEDROCK, pos(-5,0,1))
+
+
+
+
+player.say("your order is completed");
+});
